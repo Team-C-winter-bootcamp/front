a53d7a5f1fa433f3fbf72860668c43c8e95d8f12
seungjo-02 <binsama0106@gmail.com>
2026-01-24 23:52:03 +0900

    errorfix2

--- src/context/CaseContext.tsx
+++ src/context/CaseContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useContext, ReactNode } from 'react';
+import { useState, createContext, useContext, ReactNode } from 'react';
 
 export type CaseType = 'loan' | 'lease' | 'damages' | 'other' | null;
 
